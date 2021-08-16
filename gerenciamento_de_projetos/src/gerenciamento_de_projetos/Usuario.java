357289521686ecb9227a61735dbb38e7550a9cb0
pedropaulowski <ixf34rxi@gmail.com>
2021-08-15 22:08:12 -0300

    Classe usuario criada

---
+++ gerenciamento_de_projetos/src/gerenciamento_de_projetos/Usuario.java
@@ -0,0 +1,41 @@
+package gerenciamento_de_projetos;
+
+public class Usuario {
+	public String id;
+	public String nome;
+	public boolean administrador;
+	
+	public Usuario(String id, String nome, boolean administrador) {
+		this.id = id;
+		this.nome = nome;
+		this.administrador = administrador;
+	}
+	
+	public String getID() {
+		return this.id;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public boolean getAdministrador() {
+		return this.administrador;
+	}
+	
+	public void setID(String id) {
+		this.id = id;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setAdministrador() {
+		this.administrador = true;
+	}
+	
+	public void unsetAdministrador() {
+		this.administrador = false;
+	}
+}
